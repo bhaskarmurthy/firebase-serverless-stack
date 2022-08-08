d4d92983f8e4f2c5cd643079f80fca7d55501698
Bhaskar Murthy <bhaskar@murthy.ca>
2022-08-08 02:28:33 -0400

    chore: remove compiled files

--- packages/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.user = void 0;
-exports.user = require("./user");
-//# sourceMappingURL=index.js.map
--- packages/models/user.d.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id: string;
-    email?: string;
-    name?: string;
-}
-export declare const users: import("firebase/firestore").CollectionReference<User>;
